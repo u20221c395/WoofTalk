d2bd6dc00f6872c6b160f318a41e2f9c5ce83dcc
aldairpc15 <u202020356@upc.edu.pe>
2025-04-19 17:15:05 -0500

    Creacion DTO Usuario
    Creacion DTO Usuario

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/DTOS/UsuarioDTO.java
@@ -0,0 +1,87 @@
+package pe.edu.upc.wooftalkv1.DTOS;
+
+import pe.edu.upc.wooftalkv1.entities.Calificacion;
+import pe.edu.upc.wooftalkv1.entities.Rol;
+
+public class UsuarioDTO {
+
+    private int id;
+
+    private String usuario;
+
+    private String contrasena;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String telefono;
+
+    private Rol rol;
+
+    private Calificacion calificacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Calificacion getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Calificacion calificacion) {
+        this.calificacion = calificacion;
+    }
+}
