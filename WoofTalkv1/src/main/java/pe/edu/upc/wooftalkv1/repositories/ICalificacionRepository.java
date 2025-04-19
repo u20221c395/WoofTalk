63dae64ddef910e17374125ecd952a9d942b0468
aldairpc15 <u202020356@upc.edu.pe>
2025-04-19 17:13:14 -0500

    Creacion Repositorio Calificacion
    Creacion Repositorio Calificacion

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/ICalificacionRepository.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.wooftalkv1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.wooftalkv1.entities.Calificacion;
+@Repository
+
+public interface ICalificacionRepository extends JpaRepository<Calificacion, Long> {
+
+}
