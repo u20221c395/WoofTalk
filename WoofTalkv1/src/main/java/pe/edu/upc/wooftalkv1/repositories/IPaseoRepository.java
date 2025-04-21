689b0779c067d792f0010a8835fdab2ef0edc843
Melissa <u20211c566@upc.edu.pe>
2025-04-21 17:52:13 -0500

    Paseo_repository

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IPaseoRepository.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IPaseoRepository.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.wooftalkv1.repositories;
 
-public interface IPaseoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.wooftalkv1.entities.Paseo;
+
+@Repository
+
+public interface IPaseoRepository extends JpaRepository<Paseo, Integer>{
 }
