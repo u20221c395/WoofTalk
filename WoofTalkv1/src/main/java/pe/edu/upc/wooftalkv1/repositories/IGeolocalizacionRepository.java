ed42e3066f836b3be003b305cb4fd5e4260bea51
Melissa <u20211c566@upc.edu.pe>
2025-04-21 17:51:08 -0500

    Geolocalizacion_repository

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IGeolocalizacionRepository.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IGeolocalizacionRepository.java
@@ -1,4 +1,8 @@
 package pe.edu.upc.wooftalkv1.repositories;
 
-public interface IGeolocalizacionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.wooftalkv1.entities.Geolocalizacion;
+@Repository
+public interface IGeolocalizacionRepository  extends JpaRepository<Geolocalizacion, Integer>{
 }
