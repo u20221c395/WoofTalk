479e41cdae62516c7c9c6df27a21f87345a1338a
John <johnluna2911@gmail.com>
2025-04-21 18:47:09 -0500

    Entities

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/MascotaEnfermedad.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/MascotaEnfermedad.java
@@ -9,4 +9,44 @@ public class MascotaEnfermedad {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @ManyToOne
+    @JoinColumn(name = "idMascota")
+    private Mascota mascota;
+
+    @ManyToOne
+    @JoinColumn(name = "idEnfermedad")
+    private Enfermedad enfermedad;
+
+    public MascotaEnfermedad() {
+    }
+
+    public MascotaEnfermedad(int id, Mascota mascota, Enfermedad enfermedad) {
+        this.id = id;
+        this.mascota = mascota;
+        this.enfermedad = enfermedad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public Enfermedad getEnfermedad() {
+        return enfermedad;
+    }
+
+    public void setEnfermedad(Enfermedad enfermedad) {
+        this.enfermedad = enfermedad;
+    }
 }
