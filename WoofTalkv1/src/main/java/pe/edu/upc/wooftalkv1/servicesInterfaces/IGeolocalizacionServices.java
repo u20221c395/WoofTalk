4fbd687a3148eff450b4a66c7cffb555073e60dd
John <johnluna2911@gmail.com>
2025-04-21 18:22:48 -0500

    Geolocalizacion

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IGeolocalizacionServices.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.wooftalkv1.servicesInterfaces;
+
+import pe.edu.upc.wooftalkv1.entities.Geolocalizacion;
+
+import java.util.List;
+
+public interface IGeolocalizacionServices {
+    public List<Geolocalizacion> list();
+    public void insert(Geolocalizacion geolocalizacion);
+}
