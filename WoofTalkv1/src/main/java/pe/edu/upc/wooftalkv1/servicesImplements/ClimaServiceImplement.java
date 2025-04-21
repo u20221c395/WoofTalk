e5a96073134e8ab6e18258283c9146496bbd8c5b
John <johnluna2911@gmail.com>
2025-04-21 10:22:00 -0500

    ClimaServiceImplement

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesImplements/ClimaServiceImplement.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.wooftalkv1.servicesImplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.wooftalkv1.entities.Clima;
+import pe.edu.upc.wooftalkv1.repositories.IClimaRepository;
+import pe.edu.upc.wooftalkv1.servicesInterfaces.IClimaServices;
+
+import java.util.List;
+
+@Service
+public class ClimaServiceImplement implements IClimaServices {
+    @Autowired
+    private IClimaRepository cR;
+
+    @Override
+    public List<Clima> list(){
+        return cR.findAll();
+    }
+
+    @Override
+    public void insert(Clima clima){
+        cR.save(clima);
+    }
+
+    @Override
+    public void update(Clima c){
+        cR.save(c);
+    }
+
+    @Override
+    public void delete(int id){
+        cR.deleteById(id);
+    }
+}
