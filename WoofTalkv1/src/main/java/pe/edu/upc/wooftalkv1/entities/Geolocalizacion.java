fff9b0a87abdc714c22d1b6407d7ddf5f7ac4b10
Melissa <u20211c566@upc.edu.pe>
2025-04-21 11:28:10 -0500

    Geolocalizacion_entities

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/Geolocalizacion.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/Geolocalizacion.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.wooftalkv1.entities;
 
+import java.util.Date;
+
 public class Geolocalizacion {
+
+    private int id;
+    private double latitud;
+    private double longitud;
+    private Date fecha;
+
+    private Clima clima;
 }
