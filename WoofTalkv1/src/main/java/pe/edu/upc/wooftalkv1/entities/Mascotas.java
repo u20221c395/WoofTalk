c32e978df47a683c3e34c39ac7b1ffe418fe1e8a
Melissa <u20211c566@upc.edu.pe>
2025-04-21 11:28:38 -0500

    Mascotas_entities

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/Mascotas.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/Mascotas.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.wooftalkv1.entities;
 
 public class Mascotas {
+
+    private int id;
+    private String nombre;
+    private String raza;
+    private int edad;
+    private String tamanio;
+    private String observaciones;
+
+    private Usuario usuario;
 }
