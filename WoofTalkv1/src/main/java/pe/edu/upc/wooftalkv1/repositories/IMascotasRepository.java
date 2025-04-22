456f03621fcc2cb7f578bffa42deea2e00e4fea6
Melissa <u20211c566@upc.edu.pe>
2025-04-21 23:11:37 -0500

    Mascota_repository

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IMascotasRepository.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IMascotasRepository.java
@@ -5,6 +5,5 @@ import org.springframework.stereotype.Repository;
 import pe.edu.upc.wooftalkv1.entities.Mascotas;
 
 @Repository
-
 public interface IMascotasRepository extends JpaRepository<Mascotas, Integer>{
 }
