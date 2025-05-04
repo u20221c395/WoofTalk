c04bd108fba82a0b51c9088191fe56513979a0bc
aldairpc15 <u202020356@upc.edu.pe>
2025-05-04 15:53:27 -0500

    Actualizacion IMascotasServices.java
    Actualizacion IMascotasServices

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IMascotasServices.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IMascotasServices.java
@@ -2,6 +2,7 @@ package pe.edu.upc.wooftalkv1.servicesInterfaces;
 
 import pe.edu.upc.wooftalkv1.entities.Clima;
 import pe.edu.upc.wooftalkv1.entities.Mascotas;
+import pe.edu.upc.wooftalkv1.entities.MetodoPago;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public interface IMascotasServices {
     public List<Mascotas> list();
     public void insert(Mascotas mascotas);
     public void update(Mascotas mascotas);
+    public Mascotas listarId(int id);
     public void delete(int id);
 
 }
