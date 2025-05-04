1b252fb349eb62a83a5c1de9678f46e4e4522f66
aldairpc15 <u202020356@upc.edu.pe>
2025-05-04 15:51:59 -0500

    Actualizacion UsuarioController.java
    Actualizacion UsuarioController

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/Controllers/UsuarioController.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/Controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.wooftalkv1.Controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.wooftalkv1.DTOS.UsuarioDTO;
 import pe.edu.upc.wooftalkv1.entities.Usuario;
@@ -18,6 +19,7 @@ public class UsuarioController {
 
     //Listar Usuario
     @GetMapping("/listar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public List<UsuarioDTO> obtenerUsuarios(){
 
         return iusuarioServices.listar().stream().map(x->{
@@ -28,6 +30,7 @@ public class UsuarioController {
 
     //Registrar Usuario
     @PostMapping("/registrar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void registrar(@RequestBody UsuarioDTO usuarioDTO) {
         ModelMapper m = new ModelMapper();
         Usuario usuario = m.map(usuarioDTO, Usuario.class);
@@ -35,7 +38,8 @@ public class UsuarioController {
     }
 
     //Listar por Id Usuario
-    @GetMapping("/{id}")
+    @GetMapping("/buscarporid/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public UsuarioDTO listarId(@PathVariable("id") Long id){
         ModelMapper m = new ModelMapper();
         UsuarioDTO userDTO=m.map(iusuarioServices.listarId(id),UsuarioDTO.class);
@@ -44,6 +48,7 @@ public class UsuarioController {
 
     //Actualizar Usuario
     @PutMapping("/actualizar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void actualizar(@RequestBody UsuarioDTO userDTO){
         ModelMapper m=new ModelMapper();
         Usuario user=m.map(userDTO,Usuario.class);
@@ -51,9 +56,11 @@ public class UsuarioController {
     }
 
     //Eliminar Usuario
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/eliminar/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void eliminar(@PathVariable("id") Long id){
         iusuarioServices.eliminar(id);
     }
 }
+
 
