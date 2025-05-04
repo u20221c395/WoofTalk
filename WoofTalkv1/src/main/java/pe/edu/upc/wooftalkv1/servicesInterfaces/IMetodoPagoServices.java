f5d451ec31d779beaeb2964e1a4f3cb14d7cb5dc
aldairpc15 <u202020356@upc.edu.pe>
2025-05-04 15:55:24 -0500

    Actualizacion IMetodoPagoServices.java
    Actualizacion IMetodoPagoServices

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IMetodoPagoServices.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IMetodoPagoServices.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.wooftalkv1.servicesInterfaces;
 
 import pe.edu.upc.wooftalkv1.entities.MetodoPago;
+import pe.edu.upc.wooftalkv1.entities.Usuario;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ public interface IMetodoPagoServices {
     public List<MetodoPago> list();
     public void insert(MetodoPago metodoPago);
     public void update(MetodoPago metodoPago);
+    public MetodoPago listarId(int id);
     public void delete(int id);
 }
 
