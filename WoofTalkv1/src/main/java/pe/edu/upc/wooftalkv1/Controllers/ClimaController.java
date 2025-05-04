904c0a53cd6e33ad20e3ab3a64bf5b50269b0b42
aldairpc15 <u202020356@upc.edu.pe>
2025-05-04 15:48:51 -0500

    Actualizacion ClimaController.java
    Actualizacion ClimaController

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/Controllers/ClimaController.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/Controllers/ClimaController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.wooftalkv1.Controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.wooftalkv1.DTOS.ClimaDTO;
 import pe.edu.upc.wooftalkv1.entities.Clima;
@@ -18,6 +19,7 @@ public class ClimaController {
     private IClimaServices cS;
 
     @GetMapping("/listar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public List<Clima> listar() {
         return cS.list().stream().map(x->{
             ModelMapper m = new ModelMapper();
@@ -25,22 +27,34 @@ public class ClimaController {
         }).collect(Collectors.toList());
     }
 
-    @PostMapping("/agregar")
+    @PostMapping("/registrar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void agregar(@RequestBody ClimaDTO climaDTO){
         ModelMapper m = new ModelMapper();
         Clima c = m.map(climaDTO,Clima.class);
         cS.insert(c);
     }
 
-    @PutMapping
+    @PutMapping("/actualizar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void modificar(@RequestBody ClimaDTO climaDTO){
         ModelMapper m = new ModelMapper();
         Clima c = m.map(climaDTO,Clima.class);
         cS.update(c);
     }
 
-    @DeleteMapping("{id}")
+    @GetMapping("/buscarporid/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
+    public ClimaDTO listarId(@PathVariable("id") int id){
+        ModelMapper m = new ModelMapper();
+        ClimaDTO climaDTO=m.map(cS.listarId(id),ClimaDTO.class);
+        return climaDTO;
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void eliminar(@PathVariable("id") int id){
         cS.delete(id);
     }
 }
+
