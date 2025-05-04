a92ebbabcfd10dad756aec183366b9324b41eb5f
aldairpc15 <u202020356@upc.edu.pe>
2025-05-04 15:50:37 -0500

    Actualizacion MetodoPagoController.java

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/Controllers/MetodoPagoController.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/Controllers/MetodoPagoController.java
@@ -2,9 +2,11 @@ package pe.edu.upc.wooftalkv1.Controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.wooftalkv1.DTOS.ClimaDTO;
 import pe.edu.upc.wooftalkv1.DTOS.MetodoPagoDTO;
+import pe.edu.upc.wooftalkv1.DTOS.UsuarioDTO;
 import pe.edu.upc.wooftalkv1.entities.Clima;
 
 import pe.edu.upc.wooftalkv1.entities.MetodoPago;
@@ -21,6 +23,7 @@ public class MetodoPagoController {
     private IMetodoPagoServices mpS;
 
     @GetMapping("/listar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public List<MetodoPagoDTO> listar() {
         return mpS.list().stream().map(x->{
             ModelMapper m = new ModelMapper();
@@ -28,21 +31,32 @@ public class MetodoPagoController {
         }).collect(Collectors.toList());
     }
 
-    @PostMapping("/agregar")
+    @PostMapping("/registrar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void agregar(@RequestBody MetodoPagoDTO metodoPagoDTO){
         ModelMapper m = new ModelMapper();
         MetodoPago mp = m.map(metodoPagoDTO,MetodoPago.class);
         mpS.insert(mp);
     }
 
-    @PutMapping
+    @PutMapping("/actualizar")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void modificar(@RequestBody MetodoPagoDTO metodoPagoDTO){
         ModelMapper m = new ModelMapper();
         MetodoPago mp = m.map(metodoPagoDTO,MetodoPago.class);
         mpS.update(mp);
     }
 
-    @DeleteMapping("{id}")
+    @GetMapping("/buscarporid/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
+    public MetodoPagoDTO listarId(@PathVariable("id") int id) {
+        ModelMapper m = new ModelMapper();
+        MetodoPagoDTO metodoPagoDTO = m.map(mpS.listarId(id), MetodoPagoDTO.class);
+        return metodoPagoDTO;
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR')")
     public void eliminar(@PathVariable("id") int id){
         mpS.delete(id);
     }
