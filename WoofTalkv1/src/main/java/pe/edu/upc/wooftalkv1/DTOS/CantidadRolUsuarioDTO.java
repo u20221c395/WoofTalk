d4ef60ac55da2b325d9526338f5b2c5aac3c7fbe
u20221c395 <u20221c395@upc.edu.pe>
2025-05-04 23:45:40 -0500

    Add files via upload

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/DTOS/CantidadRolUsuarioDTO.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.wooftalkv1.DTOS;
+
+public class CantidadRolUsuarioDTO {
+    private int cantidad;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
