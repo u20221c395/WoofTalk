8568ba1a131772e02a9baec5a79108aa242ee948
Melissa <u20211c566@upc.edu.pe>
2025-05-05 17:03:03 -0500

    correccion IMascotasRepository

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IMascotasRepository.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IMascotasRepository.java
@@ -1,14 +1,18 @@
 package pe.edu.upc.wooftalkv1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pe.edu.upc.wooftalkv1.DTOS.MascotasporDuenoDTO;
 import pe.edu.upc.wooftalkv1.entities.Mascotas;
+
+import java.util.List;
 
 @Repository
 public interface IMascotasRepository extends JpaRepository<Mascotas, Integer>{
 
-      @Query("SELECT m FROM Mascotas m WHERE m.edad > 10")
-    List<Mascotas> Mascotasconedadmasde10();
+      @Query(" SELECT m FROM Mascotas m WHERE m.edad > 10 ")
+    public List<Mascotas> Mascotasconedadmasde10();
 
       @Query("SELECT new pe.edu.upc.wooftalkv1.DTOS.MascotasporDuenoDTO(CONCAT(u.nombre, ' ', u.apellido), COUNT(m)) " +
             "FROM Mascotas m JOIN m.usuario u GROUP BY u.nombre, u.apellido")
