9c9c7fd49cf5c15593f3004afbde2dfb1e93c952
aldairpc15 <u202020356@upc.edu.pe>
2025-05-04 21:25:14 -0500

    Querie 2 MascotasRepository.java
    Querie 2

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IMascotasRepository.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IMascotasRepository.java
@@ -10,5 +10,7 @@ public interface IMascotasRepository extends JpaRepository<Mascotas, Integer>{
       @Query("SELECT m FROM Mascotas m WHERE m.edad > 10")
     List<Mascotas> Mascotasconedadmasde10();
 
-  
+      @Query("SELECT new pe.edu.upc.wooftalkv1.DTOS.MascotasporDuenoDTO(CONCAT(u.nombre, ' ', u.apellido), COUNT(m)) " +
+            "FROM Mascotas m JOIN m.usuario u GROUP BY u.nombre, u.apellido")
+    public List<MascotasporDuenoDTO> mascotastotalporDueno();
 }
