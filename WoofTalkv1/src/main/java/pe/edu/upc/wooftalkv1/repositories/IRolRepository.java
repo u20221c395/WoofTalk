339de1c8eb309da4bb987442da67e17d6816646f
u20221c395 <u20221c395@upc.edu.pe>
2025-05-04 23:41:13 -0500

    Update IRolRepository.java

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IRolRepository.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/repositories/IRolRepository.java
@@ -1,9 +1,17 @@
 package pe.edu.upc.wooftalkv1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.wooftalkv1.entities.Rol;
 
+import java.util.List;
+
 @Repository
 public interface IRolRepository extends JpaRepository<Rol, Integer> {
+    @Query(value = " SELECT COUNT(*) \n" +
+            " FROM usuario\n" +
+            " WHERE rol_id = 2;",nativeQuery = true)
+
+    public List<String[]> cantidadTotalDeRolUsuariosRegistrados();
 }
