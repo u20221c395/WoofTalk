11828f4a307a8ef3e4fff6571f9596a607b96216
Melissa <u20211c566@upc.edu.pe>
2025-05-05 22:39:39 -0500

    Contrato creacion

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/entities/Contrato.java
@@ -0,0 +1,90 @@
+package pe.edu.upc.wooftalkv1.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Contrato")
+public class Contrato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_Contrato;
+
+    @Column(name = "Fecha_inicio", nullable = false)
+    private Date fecha_inicio;
+
+    @Column(name = "Fecha_fin", nullable = false)
+    private Date fecha_fin;
+
+    @Column(name = "Monto", nullable = false)
+    private double monto;
+
+    @ManyToOne
+    @JoinColumn(name = "Id_mascotas")
+    private Mascotas mascotas;
+
+    @ManyToOne
+    @JoinColumn(name = "Id_MetodoPago")
+    private MetodoPago metodoPago;
+
+    public Contrato() {
+    }
+
+    public Contrato(int id_Contrato, Date fecha_inicio, Date fecha_fin, double monto, Mascotas mascotas, MetodoPago metodoPago) {
+        this.id_Contrato = id_Contrato;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+        this.monto = monto;
+        this.mascotas = mascotas;
+        this.metodoPago = metodoPago;
+    }
+
+    public int getId_Contrato() {
+        return id_Contrato;
+    }
+
+    public void setId_Contrato(int id_Contrato) {
+        this.id_Contrato = id_Contrato;
+    }
+
+    public Date getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public Date getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public Mascotas getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(Mascotas mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+}
