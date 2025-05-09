1a1600dd65e247c5bbe8efb38778ec054168fcf7
Julmer <u20211c395@upc.edu.pe>
2025-05-08 23:29:16 -0500

    listar por id IEnfermedadService

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IEnfermedadServices.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesInterfaces/IEnfermedadServices.java
@@ -1,7 +1,6 @@
 package pe.edu.upc.wooftalkv1.servicesInterfaces;
 
 import pe.edu.upc.wooftalkv1.entities.Enfermedad;
-import pe.edu.upc.wooftalkv1.entities.Rol;
 
 import java.util.List;
 
@@ -9,6 +8,7 @@ public interface IEnfermedadServices {
     public List<Enfermedad> list();
     public void insert(Enfermedad enfermedad);
     public void update(Enfermedad e);
+    public Enfermedad ListarId(int id);
     public void delete(int id);
 
     public List<String[]> buscarMascotasPorEnfermedad(String name);
