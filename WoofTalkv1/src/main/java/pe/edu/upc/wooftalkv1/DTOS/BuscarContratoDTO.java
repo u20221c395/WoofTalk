8d5f2d159e2f599ecb2be6082234054d270c86b6
Melissa <u20211c566@upc.edu.pe>
2025-05-09 12:34:27 -0500

    actualizacion query

---
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/DTOS/BuscarContratoDTO.java
@@ -0,0 +1,33 @@
+package pe.edu.upc.wooftalkv1.DTOS;
+
+import java.util.Date;
+
+public class BuscarContratoDTO {
+    private int id_Contrato;
+    private Date fecha_fin;
+    private double monto;
+
+    public int getId_Contrato() {
+        return id_Contrato;
+    }
+
+    public void setId_Contrato(int id_Contrato) {
+        this.id_Contrato = id_Contrato;
+    }
+
+    public Date getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+}
