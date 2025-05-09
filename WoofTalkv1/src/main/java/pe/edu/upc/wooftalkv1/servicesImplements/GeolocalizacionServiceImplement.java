a75681bdbea7f82d983759b6820eb246b84bc393
Julmer <u20211c395@upc.edu.pe>
2025-05-09 00:07:10 -0500

    se completo el crud en GeolocalizacionServiceImplement

--- WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesImplements/GeolocalizacionServiceImplement.java
+++ WoofTalkv1/src/main/java/pe/edu/upc/wooftalkv1/servicesImplements/GeolocalizacionServiceImplement.java
@@ -9,7 +9,7 @@ import pe.edu.upc.wooftalkv1.servicesInterfaces.IGeolocalizacionServices;
 import java.util.List;
 
 @Service
-public class GeolocalizacionServiceImplement implements IGeolocalizacionServices {
+public abstract class GeolocalizacionServiceImplement implements IGeolocalizacionServices {
     @Autowired
     private IGeolocalizacionRepository gR;
 
@@ -21,5 +21,18 @@ public class GeolocalizacionServiceImplement implements IGeolocalizacionServices
     @Override
     public void insert(Geolocalizacion geolocalizacion) {
         gR.save(geolocalizacion);
+    }
+
+    @Override
+    public void update(Geolocalizacion g) {
+        gR.save(g);
+    }
+
+    @Override
+    public Geolocalizacion ListarId(int id) {return gR.findById(id).orElse(new Geolocalizacion());}
+
+    @Override
+    public void delete(int id) {
+        gR.deleteById(id);
     }
 }
